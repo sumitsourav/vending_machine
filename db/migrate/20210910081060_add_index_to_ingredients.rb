4f0a5ab737a90b769ff2dfd3a6451a4c241a68f3
sumit sourav <sumitsourav@sumits-MacBook-Air.local>
2021-09-11 19:51:50 +0530

    changing table migration name

---
+++ db/migrate/20210910081060_add_index_to_ingredients.rb
@@ -0,0 +1,5 @@
+class AddIndexToIngredients < ActiveRecord::Migration[6.1]
+  def change
+    add_index :ingredients, :name
+  end
+end
--- db/migrate/20210910081060_add_index_to_table.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToTable < ActiveRecord::Migration[6.1]
-  def change
-    add_index :ingredients, :name
-  end
-end
